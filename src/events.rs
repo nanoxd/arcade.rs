4107ad4a972c6df16b592e2c7b2a9efcfc24c8ec
Fernando Paredes <nano@fdp.io>
2016-12-30 10:23:41 -0800

    [Events] Change to a macro based keyboard identifier

--- src/events.rs
+++ src/events.rs
@@ -1,26 +1,37 @@
 extern crate sdl2;
+
 macro_rules! struct_events {
-    () => {
+    (
+        keyboard: { $( $k_alias:ident : $k_sdl:ident ),* }
+    )
+    => {
         use sdl2::EventPump;
 
-        pub struct ImmediateEvents;
+        pub struct ImmediateEvents {
+            $( pub $k_alias: Option<bool> ),*
+        }
 
         impl ImmediateEvents {
             pub fn new() -> ImmediateEvents {
-                ImmediateEvents
+                ImmediateEvents {
+                    $( $k_alias: None ),*
+                }
             }
         }
 
         pub struct Events {
             pump: EventPump,
             pub now: ImmediateEvents,
+
+            $( pub $k_alias: bool ),*
         }
 
         impl Events {
             pub fn new(pump: EventPump) -> Events {
                 Events {
                     pump: pump,
-                    now: ImmediateEvents::new()
+                    now: ImmediateEvents::new(),
+                    $( $k_alias: false ),*
                 }
             }
 
@@ -33,6 +44,31 @@ macro_rules! struct_events {
                     use sdl2::keyboard::Keycode::*;
 
                     match event {
+                        KeyDown { keycode, .. } => match keycode {
+                            $(
+                                Some($k_sdl) => {
+                                    if !self.$k_alias {
+                                        self.now.$k_alias = Some(true);
+                                    }
+
+                                    self.$k_alias = true;
+                                }
+                            ),*
+                            
+                            _ => {}
+                        },
+
+                        KeyUp { keycode, .. } => match keycode {
+                            $(
+                                Some($k_sdl) => {
+                                    self.now.$k_alias = Some(false);
+                                    self.$k_alias = false;
+                                }
+                            ),*
+
+                            _ => {}
+                        },
+
                         _ => {}
                     }
                 }
