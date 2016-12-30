2362e7c8041f9831cede252b199d91c8dc4de927
Fernando Paredes <nano@fdp.io>
2016-12-29 21:33:36 -0800

    [Events] Add pump function

--- src/events.rs
+++ src/events.rs
@@ -18,4 +18,31 @@ impl Events {
             key_escape: false,
         }
     }
+
+    /// Update the events record.
+    pub fn pump(&mut self) {
+        // If the SDL context is dropped, then poll_iter() will simply stop
+        // yielding any input.
+
+        for event in self.pump.poll_iter() {
+            use sdl2::event::Event::*;
+            use sdl2::keyboard::Keycode::*;
+
+            match event {
+                Quit { .. } => self.quit = true,
+
+                KeyDown { keycode, .. } => match keycode {
+                    Some(Escape) => self.key_escape = true,
+                    _ => {}
+                },
+
+                KeyUp { keycode, .. } => match keycode {
+                    Some(Escape) => self.key_escape = false,
+                    _ => {}
+                },
+
+                _ => {}
+            }
+        }
+    }
 }
