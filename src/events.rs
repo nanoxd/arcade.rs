7a260e06818b35bec07bec519228c41fab7fc9d6
Fernando Paredes <nano@fdp.io>
2016-12-30 10:12:35 -0800

    [Events] Wrap in macro

--- src/events.rs
+++ src/events.rs
@@ -1,37 +1,41 @@
 extern crate sdl2;
+macro_rules! struct_events {
+    () => {
+        use sdl2::EventPump;
 
-use sdl2::EventPump;
+        pub struct ImmediateEvents;
 
-pub struct ImmediateEvents;
-
-impl ImmediateEvents {
-    pub fn new() -> ImmediateEvents {
-        ImmediateEvents
-    }
-}
-
-pub struct Events {
-    pump: EventPump,
-    pub now: ImmediateEvents,
-}
-impl Events {
-    pub fn new(pump: EventPump) -> Events {
-        Events {
-            pump: pump,
-            now: ImmediateEvents::new()
+        impl ImmediateEvents {
+            pub fn new() -> ImmediateEvents {
+                ImmediateEvents
+            }
         }
-    }
 
-    /// Update the events record.
-    pub fn pump(&mut self) {
-        self.now = ImmediateEvents::new();
+        pub struct Events {
+            pump: EventPump,
+            pub now: ImmediateEvents,
+        }
 
-        for event in self.pump.poll_iter() {
-            use sdl2::event::Event::*;
-            use sdl2::keyboard::Keycode::*;
+        impl Events {
+            pub fn new(pump: EventPump) -> Events {
+                Events {
+                    pump: pump,
+                    now: ImmediateEvents::new()
+                }
+            }
 
-            match event {
-                _ => {}
+            /// Update the events record.
+            pub fn pump(&mut self) {
+                self.now = ImmediateEvents::new();
+
+                for event in self.pump.poll_iter() {
+                    use sdl2::event::Event::*;
+                    use sdl2::keyboard::Keycode::*;
+
+                    match event {
+                        _ => {}
+                    }
+                }
             }
         }
     }
