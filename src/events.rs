56ce149e6e455b25d2978c95650a05fd8b83ffff
Fernando Paredes <nano@fdp.io>
2016-12-29 21:18:20 -0800

    Add events struct

--- src/events.rs
+++ src/events.rs
@@ -1,3 +1,21 @@
 extern crate sdl2;
 
 use sdl2::EventPump;
+
+pub struct Events {
+    pump: EventPump,
+
+    pub quit: bool,
+    pub key_escape: bool,
+}
+
+impl Events {
+    pub fn new(pump: EventPump) -> Events {
+        Events {
+            pump: pump,
+
+            quit: false,
+            key_escape: false,
+        }
+    }
+}
