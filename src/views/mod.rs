dbc99ff7fe4d9b8b42e488f1e3756762844ea12f
Fernando Paredes <nano@fdp.io>
2017-01-07 12:20:50 -0800

    Add Rectangle data type

--- src/views/mod.rs
+++ src/views/mod.rs
@@ -6,6 +6,31 @@ use sdl2::rect::Rect as SdlRect;
 
 // Data types
 
+struct Ship {
+    rect: Rectangle,
+}
+
+#[derive(Clone, Copy, Debug, PartialEq)]
+pub struct Rectangle {
+    pub x: f64,
+    pub y: f64,
+    pub w: f64,
+    pub h: f64,
+}
+
+impl Rectangle {
+    /// Generates an SDL-compatible Rect equivalent to `self`.
+    /// Panics if it could not be created, for example if a
+    /// coordinate of a corner overflows an `i32`.
+    pub fn to_sdl(self) -> Option<SdlRect> {
+        // Reject negative width and height
+        assert!(self.w >= 0.0 && self.h >= 0.0);
+
+        SdlRect::new(self.x as i32, self.y as i32, self.w as u32, self.h as u32)
+            .unwrap()
+    }
+}
+
 // View Definitions
 
 pub struct ShipView;
