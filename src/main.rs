12ac28c70c2f5ea166ca78280a69186fab6ad9c1
Fernando Paredes <nano@fdp.io>
2017-01-07 10:22:29 -0800

    Add view, actions, and Phi

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ extern crate sdl2;
 mod events;
 
 use sdl2::pixels::Color;
+use sdl2::render::Renderer;
 
 struct_events! {
     keyboard: {
@@ -14,6 +15,29 @@ struct_events! {
     else: {
         quit: Quit { .. }
     }
+}
+
+/// A `ViewAction` is a way for the currently executed view to
+/// communicate with the game loop. It specifies which action
+/// should be executed before the next rendering.
+pub enum ViewAction {
+    None,
+    Quit,
+}
+
+pub trait View {
+    /// Called on every frame to take care of both the logic and
+    /// the rendering of the current view.
+    ///
+    /// `elapsed` is expressed in seconds.
+    fn render(&mut self, context: &mut Phi, elapsed: f64) -> ViewAction;
+}
+
+/// Bundles the Phi abstractions in a single structure which
+/// can be passed easily between functions.
+pub struct Phi<'a> {
+    pub events: Events,
+    pub renderer: Renderer<'a>,
 }
 
 fn main() {
