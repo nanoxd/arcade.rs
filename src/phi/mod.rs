cfd63dec9593bc1fa28b679f20493a3eeffc35e4
Fernando Paredes <nano@fdp.io>
2017-01-07 16:50:59 -0800

    Give output size of window

--- src/phi/mod.rs
+++ src/phi/mod.rs
@@ -26,6 +26,14 @@ pub struct Phi<'window> {
     pub renderer: Renderer<'window>,
 }
 
+impl<'window> Phi<'window> {
+    pub fn output_size(&self) -> (f64, f64) {
+        let (w, h) = self.renderer.output_size().unwrap();
+
+        (w as f64, h as f64)
+    }
+}
+
 /// A `ViewAction` is a way for the currently executed view to
 /// communicate with the game loop. It specifies which action
 /// should be executed before the next rendering.
