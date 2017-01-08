c0a9ddd679cbfc631d5866cfaaba750eb0336ddb
Fernando Paredes <nano@fdp.io>
2017-01-08 10:34:18 -0800

    [Sprite] add an initializer

--- src/phi/gfx.rs
+++ src/phi/gfx.rs
@@ -10,3 +10,20 @@ pub struct Sprite {
     tex: Rc<RefCell<Texture>>,
     src: Rectangle,
 }
+
+impl Sprite {
+    /// Creates a new sprite by wrapping a texture
+    pub fn new(texture: Texture) -> Sprite {
+        let tex_query = texture.query();
+
+        Sprite {
+            tex: Rc::new(RefCell::new(texture)),
+            src: Rectangle {
+                w: tex_query.width as f64,
+                h: tex_query.height as f64,
+                x: 0.0,
+                y: 0.0,
+            }
+        }
+    }
+}
